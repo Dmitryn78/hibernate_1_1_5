272f2c7b4e42aa5030448dce4309525071c7b898
Dmitry Novikov <Dmitryn78@yandex.ru>
2023-01-09 17:20:20 +0300

    Выполнил задание

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,78 @@
 package jm.task.core.jdbc.dao;
 
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 import jm.task.core.jdbc.model.User;
+
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
-
-    }
-
 
     @Override
     public void createUsersTable() {
+       try (Session session = Util.getSessionFactory().getCurrentSession()) {
 
+           session.beginTransaction();
+           session.createSQLQuery("CREATE TABLE IF NOT EXISTS User " +
+                   "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                   "name VARCHAR(15) NOT NULL, lastName VARCHAR(20) NOT NULL, " +
+                   "age TINYINT NOT NULL)").executeUpdate();
+           session.getTransaction().commit();
+       }
     }
 
-    @Override
-    public void dropUsersTable() {
 
-    }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (Session session = Util.getSessionFactory().getCurrentSession()) {
 
+            User user = new User(name, lastName, age);
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try ( Session session = Util.getSessionFactory().getCurrentSession()) {
 
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getSessionFactory().getCurrentSession()) {
+
+            session.beginTransaction();
+            List<User> userList = session.createQuery("from User").getResultList();
+            session.getTransaction().commit();
+            return userList;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+       try (Session session = Util.getSessionFactory().getCurrentSession()) {
 
+           session.beginTransaction();
+           session.createQuery("delete User").executeUpdate();
+           session.getTransaction().commit();
+       }
+    }
+    @Override
+    public void dropUsersTable() {
+        try (Session session = Util.getSessionFactory().getCurrentSession()){
+
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS User").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,42 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+    private static SessionFactory factory;
+
+    public static SessionFactory getSessionFactory() {
+        if (factory == null) {
+            Configuration configuration = new Configuration();
+            Properties properties = new Properties();
+            properties.put(Environment.URL, "jdbc:mysql://localhost:3306/myschema");
+            properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            properties.put(Environment.USER, "root");
+            properties.put(Environment.PASS, "26Dr04!78!");
+            properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+            properties.put(Environment.SHOW_SQL, "true");
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            factory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return factory;
+    }
+
     private static final String URL = "jdbc:mysql://localhost:3306/myschema";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "26Dr04!78!";
